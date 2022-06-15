87263da8fc150ca710ba91decdddfa70fcebf819
Jaz Kime <JazDKime@gmail.com>
2022-06-15 15:14:07 -0700

    Clean up

--- src/components/Start.js
+++ src/components/Start.js
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom'
 
 class Start extends React.Component {
     render() {
-        console.log(this.props)
         return(
             <>
             {this.props.currentUser ? 
